3d6aca9d91f3ee8a76b837821ed8ba4460bf72bf
Dany <danymike.joseph@gmail.com>
2022-06-20 16:13:13 +0200

    add foreach loops on books done

--- src/main/java/com/example/demo/controllers/BookController.java
+++ src/main/java/com/example/demo/controllers/BookController.java
@@ -1,20 +1,9 @@
 package com.example.demo.controllers;
 
-import java.util.Optional;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-
-import com.example.demo.models.Book;
-import com.example.demo.service.BookService;
 
 @Controller
 public class BookController {
-
     // @GetMapping("/books/{id}")
     // public Optional<Book> getProductById(@PathVariable Long id) {
     // return bookService.getBookById(id);
