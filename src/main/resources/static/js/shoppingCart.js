8034c987d45ab0165e3cd10a6753cf32ed9936c4
Dany <danymike.joseph@gmail.com>
2022-06-21 11:35:09 +0200

    add js logic into shoppingCart.js

--- src/main/resources/static/js/shoppingCart.js
+++ src/main/resources/static/js/shoppingCart.js
@@ -1,6 +1,36 @@
 $(document).ready(function () {
+  $(".minusButton").on("click", function (e) {
+    e.preventDefault();
+    decreaseQuantity($(this));
+  });
+
+  $(".plusButton").on("click", function (e) {
+    e.preventDefault();
+    increaseQuantity($(this));
+  });
   updateTotal();
 });
+
+function decreaseQuantity(link) {
+  productId = link.attr("pid");
+  qtyInput = $("#quantity" + productId);
+
+  newQty = parseInt(qtyInput.val()) - 1;
+  console.log(newQty);
+  if (newQty > 0) {
+    updateQuantity();
+  }
+}
+
+function increaseQuantity(link) {
+  productId = link.attr("pid");
+  qtyInput = $("#quantity" + productId);
+
+  newQty = parseInt(qtyInput.val()) + 1;
+  if (newQty <= 8) qtyInput.val(newQty);
+}
+
+function updateQuantity() {}
 
 function updateTotal() {
   total = 0.0;
